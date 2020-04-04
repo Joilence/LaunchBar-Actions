38449deb5511af71bf6ef09cbe67ce0f48400ca9
Joilence <cjyk84719@gmail.com>
2020-04-04 23:07:37 +0200

    Merge with LaunchBar Actions Dir

--- Paste as MD Link.lbaction/Contents/Scripts/default.js
+++ Paste as MD Link.lbaction/Contents/Scripts/default.js
@@ -15,24 +15,50 @@ function run(argument) {
     // Get title from the URL
     try {
         if (!isValidUrl(url)) throw new Error(url + ' is not a URL.');
-        
         if (!/^https?:\/\//i.test(url)) url = 'http://' + url;
-        
-        const result = HTTP.get(url);
+
+        LaunchBar.displayNotification({
+            title: "Paste as MD Link",
+            string: "Request title from " + url
+        });
+        LaunchBar.debugLog('Start requesting.');
+        var result = HTTP.loadRequest(url, {
+            timeout: 8,
+            method: 'GET',
+            resultType: 'text'
+        });
+        LaunchBar.debugLog('Finish requesting.');
         LaunchBar.debugLog("Result: ", Object.keys(result));
         if (result.data != undefined) {
-            const matches = result.data.match(/<head>[\s\S]*<title>\s*(.*)\s*<\/title>[\s\S]*<\/head>/);
-            LaunchBar.debugLog('matches', matches[1]);
+            LaunchBar.debugLog(result.data);
+            LaunchBar.debugLog("Start matching.");
+            const matches = result.data.match(/<head[\s\S]*?>[\s\S]*?<title[\s\S]*?>\s*(.*)\s*<\/title>[\s\S]*?<\/head>/);
+            LaunchBar.debugLog("Finish matching.");
+            // LaunchBar.log('matches', matches[1]);
             const title = matches[1];
             const mdLink = '[' + title + '](' + url + ')';
+            LaunchBar.debugLog('mdLink', mdLink);
+            // LaunchBar.displayNotification({
+            //     title: "Paste as MD Link",
+            //     string: "Success!"
+            // });
             LaunchBar.paste(mdLink);
         } else if (result.timedOut) {
-            return ['Unable to load the LaunchBar page due to a timeout']
+            LaunchBar.displayNotification({
+                title: "Paste as MD Link",
+                string: "Error: " + 'Unable to load the LaunchBar page due to a timeout'
+            });
         } else if (result.error != undefined) {
-            return ['Unable to load the LaunchBar page:' + result.error]
+            LaunchBar.displayNotification({
+                title: "Paste as MD Link",
+                string: "Error: " + 'Unable to load the LaunchBar page:' + result.error
+            });
         }
     } catch (e) {
-        LaunchBar.log('ERROR: ' + e);
-        LaunchBar.alert('ERROR: ' + e);
+        LaunchBar.displayNotification({
+            title: "Paste as MD Link",
+            string: "" + e
+        });
+        LaunchBar.debugLog('' + e);
     }
 }
